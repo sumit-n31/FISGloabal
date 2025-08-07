814c162559cdd914b9e165662f3d7d3779952bec
@sumit-n31! <srn3112@gmail.com>
2025-08-07 21:47:23 +0530

    pushing uitest & apitest

---
+++ src/test/java/com/apitest/Validategetapi.java
@@ -0,0 +1,19 @@
+package com.apitest;
+
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import org.testng.annotations.Test;
+
+import io.restassured.
+
+public class Validategetapi {
+
+	@Test
+	public static void validateGetcall() {
+		given()
+		when()
+		then().
+
+	}
+
+}
