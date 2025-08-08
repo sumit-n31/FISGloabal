b31f7cfe20027bfd6c01e8c56af1b9d013338868
@sumit-n31! <srn3112@gmail.com>
2025-08-08 20:49:04 +0530

    adding code for UI test

--- src/test/java/com/apitest/Validategetapi.java
+++ src/test/java/com/apitest/Validategetapi.java
@@ -1,18 +1,16 @@
 package com.apitest;
 
-import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
 import org.testng.annotations.Test;
-
-import io.restassured.
+import org.testng.Assert;
+import io.restassured.response.Response;
 
 public class Validategetapi {
 
 	@Test
 	public static void validateGetcall() {
-		given()
-		when()
-		then().
+		Response res = given().get("https://api.coindesk.com/v1/bpi/currentprice.json");
+		
 
 	}
 
